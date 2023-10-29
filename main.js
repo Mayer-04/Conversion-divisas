3952fcf1af67f2dc147a3f88848483f0fb9909b4
Mayer Andres <andres.20.chaves@gmail.com>
2023-10-28 20:05:22 -0500

    Agregando paises y código al archivo main

--- main.js
+++ main.js
@@ -1,2 +1,8 @@
 import "./style.css";
-console.log("Hello World!");
+
+const currenciesContainer = document.getElementById("currencies");
+const inputAmount = document.getElementById("amount");
+const selectFrom = document.getElementById("select-from");
+const selectTo = document.getElementById("select-to");
+const switchCurrency = document.querySelector(".switch-currency");
+const conversionResult = document.querySelector(".conversion-result");
